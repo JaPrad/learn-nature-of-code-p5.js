a5a98161d428e60fe86bfce56e0a9bf6b7539cd9
Jyotiraditya Pradhan <japsim6dev@gmail.com>
2021-12-27 17:46:00 +0530

    Create sketch.js

---
+++ vectors/random-vectors-design/sketch.js
@@ -0,0 +1,16 @@
+let p
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+}
+
+function draw() {
+  translate(width/2, height/2)
+  p = p5.Vector.random2D();
+  p = p.mult(random(50,100));
+  
+  stroke(255);
+  strokeWeight(2);
+  line(0, 0, p.x, p.y);
+}
